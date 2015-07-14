8e16d01a34baf847f28f21712dad3189f20db75c
Zhenbin Wu <benwu@fnal.gov>
2015-07-14 12:22:59 -0500

    Workable script for condor

--- Tool/Condor/SubmitLPC.py
+++ Tool/Condor/SubmitLPC.py
@@ -4,44 +4,45 @@
 import os
 import re
 import shutil
+import time
 import subprocess
 import glob
 
-DelExe    = '../testMain'
-OutDir = '/eos/uscms/store/user/benwu/TP/Puppi/Sep24'
+DelExe    = '../testMain2'
+OutDir = '/eos/uscms/store/user/benwu/Stop/TopTagger'
 
 tempdir = ''
 UserEMAIL = 'benwu@fnal.gov'
-ProjectName = 'test1'
+ProjectName = 'test2'
 Process = {
-    'DYJetsToLL_HT_100to200'                     : ['../FileList/DYJetsToLL_HT_100to200.list',                     1],
-    'DYJetsToLL_HT_200to400'                     : ['../FileList/DYJetsToLL_HT_200to400.list',                     1],
-    'DYJetsToLL_HT_400to600'                     : ['../FileList/DYJetsToLL_HT_400to600.list',                     1],
-    'DYJetsToLL_HT_600toInf'                     : ['../FileList/DYJetsToLL_HT_600toInf.list',                     1],
-    'Signal_T1bbbb_mGluino1000_mLSP900'          : ['../FileList/Signal_T1bbbb_mGluino1000_mLSP900.list',          1],
-    'Signal_T1bbbb_mGluino1500_mLSP100'          : ['../FileList/Signal_T1bbbb_mGluino1500_mLSP100.list',          1],
-    'Signal_T1tttt_mGluino1200_mLSP800'          : ['../FileList/Signal_T1tttt_mGluino1200_mLSP800.list',          1],
-    'Signal_T1tttt_mGluino1500_mLSP100'          : ['../FileList/Signal_T1tttt_mGluino1500_mLSP100.list',          1],
-    'Signal_T2bb_mSbottom600_mLSP580'            : ['../FileList/Signal_T2bb_mSbottom600_mLSP580.list',            1],
-    'Signal_T2bb_mSbottom900_mLSP100'            : ['../FileList/Signal_T2bb_mSbottom900_mLSP100.list',            1],
-    'Signal_T2tt_mStop425_mLSP325'               : ['../FileList/Signal_T2tt_mStop425_mLSP325.list',               1],
-    'Signal_T2tt_mStop500_mLSP325'               : ['../FileList/Signal_T2tt_mStop500_mLSP325.list',               1],
-    'Signal_T2tt_mStop650_mLSP325'               : ['../FileList/Signal_T2tt_mStop650_mLSP325.list',               1],
-    'Signal_T2tt_mStop850_mLSP100'               : ['../FileList/Signal_T2tt_mStop850_mLSP100.list',               1],
-    'Signal_T5tttt_mGluino1300_mStop300_mCh285'  : ['../FileList/Signal_T5tttt_mGluino1300_mStop300_mCh285.list',  1],
-    'Signal_T5tttt_mGluino1300_mStop300_mChi280' : ['../FileList/Signal_T5tttt_mGluino1300_mStop300_mChi280.list', 1],
-    'TTZ'                                        : ['../FileList/TTZ.list',                                        1],
-    'TTbar'                                      : ['../FileList/TTbar.list',                                      1],
-    'T_tW'                                       : ['../FileList/T_tW.list',                                       2],
-    'Tbar_tW'                                    : ['../FileList/Tbar_tW.list',                                    1],
-    'WJetsToLNu_HT_100to200'                     : ['../FileList/WJetsToLNu_HT_100to200.list',                     1],
-    'WJetsToLNu_HT_200to400'                     : ['../FileList/WJetsToLNu_HT_200to400.list',                     1],
-    'WJetsToLNu_HT_400to600'                     : ['../FileList/WJetsToLNu_HT_400to600.list',                     1],
-    'WJetsToLNu_HT_600toInf'                     : ['../FileList/WJetsToLNu_HT_600toInf.list',                     1],
-    'ZJetsToNuNu_HT_100to200'                    : ['../FileList/ZJetsToNuNu_HT_100to200.list',                    1],
-    'ZJetsToNuNu_HT_200to400'                    : ['../FileList/ZJetsToNuNu_HT_200to400.list',                    1],
-    'ZJetsToNuNu_HT_400to600'                    : ['../FileList/ZJetsToNuNu_HT_400to600.list',                    1],
-    'ZJetsToNuNu_HT_600toInf'                    : ['../FileList/ZJetsToNuNu_HT_600toInf.list',                    1],
+    #'DYJetsToLL_HT_100to200'                     : ['../FileList/DYJetsToLL_HT_100to200.list',                     1],
+    #'DYJetsToLL_HT_200to400'                     : ['../FileList/DYJetsToLL_HT_200to400.list',                     1],
+    #'DYJetsToLL_HT_400to600'                     : ['../FileList/DYJetsToLL_HT_400to600.list',                     1],
+    #'DYJetsToLL_HT_600toInf'                     : ['../FileList/DYJetsToLL_HT_600toInf.list',                     1],
+    #'Signal_T1bbbb_mGluino1000_mLSP900'          : ['../FileList/Signal_T1bbbb_mGluino1000_mLSP900.list',          1],
+    #'Signal_T1bbbb_mGluino1500_mLSP100'          : ['../FileList/Signal_T1bbbb_mGluino1500_mLSP100.list',          1],
+    #'Signal_T1tttt_mGluino1200_mLSP800'          : ['../FileList/Signal_T1tttt_mGluino1200_mLSP800.list',          1],
+    #'Signal_T1tttt_mGluino1500_mLSP100'          : ['../FileList/Signal_T1tttt_mGluino1500_mLSP100.list',          1],
+    #'Signal_T2bb_mSbottom600_mLSP580'            : ['../FileList/Signal_T2bb_mSbottom600_mLSP580.list',            1],
+    #'Signal_T2bb_mSbottom900_mLSP100'            : ['../FileList/Signal_T2bb_mSbottom900_mLSP100.list',            1],
+    #'Signal_T2tt_mStop425_mLSP325'               : ['../FileList/Signal_T2tt_mStop425_mLSP325.list',               1],
+    #'Signal_T2tt_mStop500_mLSP325'               : ['../FileList/Signal_T2tt_mStop500_mLSP325.list',               1],
+    #'Signal_T2tt_mStop650_mLSP325'               : ['../FileList/Signal_T2tt_mStop650_mLSP325.list',               1],
+    #'Signal_T2tt_mStop850_mLSP100'               : ['../FileList/Signal_T2tt_mStop850_mLSP100.list',               1],
+    #'Signal_T5tttt_mGluino1300_mStop300_mCh285'  : ['../FileList/Signal_T5tttt_mGluino1300_mStop300_mCh285.list',  1],
+    #'Signal_T5tttt_mGluino1300_mStop300_mChi280' : ['../FileList/Signal_T5tttt_mGluino1300_mStop300_mChi280.list', 1],
+    #'TTZ'                                        : ['../FileList/TTZ.list',                                        1],
+    'TTbar'                                      : ['../FileList/TTbar.list',                                      2],
+    #'T_tW'                                       : ['../FileList/T_tW.list',                                       2],
+    #'Tbar_tW'                                    : ['../FileList/Tbar_tW.list',                                    1],
+    #'WJetsToLNu_HT_100to200'                     : ['../FileList/WJetsToLNu_HT_100to200.list',                     1],
+    #'WJetsToLNu_HT_200to400'                     : ['../FileList/WJetsToLNu_HT_200to400.list',                     1],
+    #'WJetsToLNu_HT_400to600'                     : ['../FileList/WJetsToLNu_HT_400to600.list',                     1],
+    #'WJetsToLNu_HT_600toInf'                     : ['../FileList/WJetsToLNu_HT_600toInf.list',                     1],
+    #'ZJetsToNuNu_HT_100to200'                    : ['../FileList/ZJetsToNuNu_HT_100to200.list',                    1],
+    #'ZJetsToNuNu_HT_200to400'                    : ['../FileList/ZJetsToNuNu_HT_200to400.list',                    1],
+    #'ZJetsToNuNu_HT_400to600'                    : ['../FileList/ZJetsToNuNu_HT_400to600.list',                    1],
+    #'ZJetsToNuNu_HT_600toInf'                    : ['../FileList/ZJetsToNuNu_HT_600toInf.list',                    1],
 }
 
 
@@ -56,17 +57,17 @@ def Condor_Sub(condor_file):
         hasproxy = False
     if not hasproxy or not os.path.exists(proxyfile) or (time.time() - os.stat(proxyfile).st_ctime) / 60/24 > 1:
         print "Proxy file is at least one day old. Requesting new proxy"
-        #os.system("voms-proxy-init -valid 168:00 -voms cms")
+        os.system("voms-proxy-init -valid 168:00 -voms cms")
 
     curdir = os.path.curdir
     os.chdir(os.path.dirname(condor_file))
-    print "condor_submit " + condor_file
-    #os.system("condor_submit " + condor_file)
+    print "To submit condor with " + condor_file
+    os.system("condor_submit " + condor_file)
     os.chdir(curdir)
 
 
 def SplitPro(key, file, fraction):
-    filelistdir = tempdir + '/' + "Filelist"
+    filelistdir = tempdir + '/' + "FileList"
     try:
         os.makedirs(filelistdir)
     except OSError:
@@ -101,7 +102,7 @@ def my_process():
         pass
 
     ## Create the output directory
-    outdir = OutDir +  "/ " + ProjectName + "/"
+    outdir = OutDir +  "/" + ProjectName + "/"
     try:
         os.makedirs(outdir)
     except OSError:
@@ -111,21 +112,24 @@ def my_process():
     RunHTFile = tempdir + "/" + "RunExe.csh"
     with open(RunHTFile, "wt") as outfile:
         for line in open("RunExe.csh", "r"):
-            line = line.replace("DELDIR", os.environ['PWD'])
-            #line = line.replace("DELDIR", os.environ['CMSSW_BASE'])
+            #line = line.replace("DELDIR", os.environ['PWD'])
+            line = line.replace("DELDIR", os.environ['CMSSW_BASE'])
             line = line.replace("DELEXE", DelExe.split('/')[-1])
+            line = line.replace("OUTDIR", outdir)
             outfile.write(line)
 
     ### Update condor files
     arg = ''
     for key, value in Process.items():
+        if not os.path.isfile(value[0]):
+            continue
         npro = GetProcess(key, value)
         if npro > 1:
             arg += "\nArguments = %s.$(Process).list %s_$(Process).root \n Queue %d \n" % (key, key, npro)
         else:
             arg += "\nArguments = %s.list %s.root \n Queue\n" % (key, key)
 
-    subprocess.call("tar -czf %s/FileList.tgz -C %s/FileList/ ." % (tempdir, tempdir), shell =True )
+    subprocess.call("tar -czf %s/FileList.tgz -C %s/FileList/ \." % (tempdir, tempdir), shell =True )
 
     tranferfiles = "%s/FileList.tgz, " %  tempdir  ## filelist
     tranferfiles += "%s, " %  os.path.abspath(DelExe)
@@ -142,10 +146,6 @@ def my_process():
             outfile.write(line)
 
     Condor_Sub(condorfile)
-
-    #os.chdir(outdir)
-    #os.system("tar -xzvf FileList.tgz")
-    #Condor_Sub()
 
 def GetProcess(key, value):
     if len(value) == 1:
